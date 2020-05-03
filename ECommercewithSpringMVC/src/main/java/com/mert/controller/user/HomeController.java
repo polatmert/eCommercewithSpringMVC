ac6fdd9f260ba95727864279660feb5279383467
Hüsnü Mert POLAT <husnumertpolatt@gmail.com>
2020-05-04 01:06:59 +0300

    index.jsp  and template_user.jsp added.

---
+++ ECommercewithSpringMVC/src/main/java/com/mert/controller/user/HomeController.java
@@ -0,0 +1,10 @@
+package com.mert.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
+public class HomeController {
+
+}
